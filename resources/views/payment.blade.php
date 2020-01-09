698ad2209eb907c379781bb1392c9003fbbb877a
j-grynkiewicz <58746888+j-grynkiewicz@users.noreply.github.com>
2020-01-09 08:11:19 +0100

    Laravel Project

---
+++ resources/views/payment.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            <a href="/home">Wróć</a><br>
+                            nr konta: 1234 5678 9828 2132 0000 000
+                            <br>
+                            Dane przelewu:xxxxx<br>
+                            Imie:xxxxxx<br>
+                            Nazwisko:xxxxxx<br>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('/payment', function () {
+    return view('payment');
+});
+Route::get('/coaches', function () {
+    return view('coaches');
+});
+Route::get('/fields', function () {
+    return view('fields');
+});
